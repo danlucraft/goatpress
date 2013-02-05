0ce4bfc72fa414012243789a91ad4a9e9ab8f1f9
Daniel Lucraft <dan@fluentradical.com>
2013-02-05 09:33:22 +0000

    BoardGenerator can make boards (randomly generated letters from random words)

--- board.go
+++ board.go
@@ -3,28 +3,99 @@ package goatpress
 import (
   "io/ioutil"
   "strings"
+  "math/rand"
 )
 
-type Move struct {
-  Squares []int
-}
+const defaultDataPath string = "/Users/dan/Dropbox/projects/go/src/goatpress/data/words"
 
 type Board struct {
-  Letters [][]int
-  Moves []Move
+  Size    int
+  Letters [][]string
 }
 
-type BoardGenerator struct {
-  Size int
-  Words []string
+// *** WordSet
+
+type WordSet interface {
+  Add(string)
+  Includes(string) bool
+  ChooseRandom() string
+  Length() int
 }
 
-func wordFileBoardGenerator(size int, path string) *BoardGenerator {
+type HashWordSet struct {
+  words map[string]bool
+  words2 []string
+}
+
+func newWordSet() *HashWordSet {
+  return &HashWordSet{make(map[string]bool), make([]string, 0)}
+}
+
+func (set *HashWordSet) Add(word string) {
+  set.words[word] = true
+  set.words2 = append(set.words2, word)
+}
+
+func (set *HashWordSet) Includes(word string) bool {
+  _, found := set.words[word]
+  return found
+}
+
+func (set *HashWordSet) ChooseRandom() string {
+  return set.words2[rand.Intn(len(set.words2))]
+}
+
+func (set *HashWordSet) Length() int {
+  return len(set.words2)
+}
+
+func newWordSetFromFile(path string) WordSet {
   b, err := ioutil.ReadFile(path)
   if err != nil {
     panic(err)
   }
-  words := strings.Split(string(b), "\n")
-  words = words[:len(words)-1]
-  return &BoardGenerator{size, words}
+  allWords := strings.Split(string(b), "\n")
+  allWords = allWords[:len(allWords)-1]
+  // strip words with uppercase in them and shorter than 2 characters
+  words := newWordSet()
+  for _, w := range allWords {
+    if w == strings.ToLower(w) && len(w) > 1 {
+      words.Add(w)
+    }
+  }
+  return words
 }
+
+// *** BoardGenerator
+
+type BoardGenerator struct {
+  Words WordSet
+}
+
+func defaultBoardGenerator() *BoardGenerator {
+  return newBoardGenerator(newWordSetFromFile(defaultDataPath))
+}
+
+func newBoardGenerator(words WordSet) *BoardGenerator {
+  return &BoardGenerator{words}
+}
+
+func (bg *BoardGenerator) newBoard(size int) *Board {
+  letters := make([][]string, size)
+  for i := 0; i < size; i++ {
+    letters[i] = make([]string, size)
+  }
+  current := 0
+  for current < size*size {
+    word := bg.Words.ChooseRandom()
+    for _, char := range word {
+      if current < size*size {
+        letters[current / 5][current % 5] = string(char)
+      }
+      current += 1
+    }
+  }
+
+  return &Board{size, letters}
+}
+
--- board_test.go
+++ board_test.go
@@ -4,21 +4,43 @@ import (
   "testing"
 )
 
-func TestBoardGenerator(t *testing.T) {
-  board := BoardGenerator{5, make([]string, 2)}
+func TestNewWordSetFromFile(t *testing.T) {
+  set := newWordSetFromFile(defaultDataPath)
+  if !set.Includes("aa") { t.Errorf("wordSet doesn't include aa") }
+  if set.Includes("a") { t.Errorf("wordSet includes a") }
+
+  if set.Length() != 210661 {
+    t.Errorf("wordSet not right length", set.Length(), 210661)
+  }
+}
+
+func TestMakingNewBoards(t *testing.T) {
+  bg := defaultBoardGenerator()
+  board := bg.newBoard(5)
   if board.Size != 5 {
-    t.Errorf("board.Size", board.Size, 5)
+    t.Errorf("board.Size is not right", board.Size, 5)
+  }
+  if len(board.Letters) != 5 {
+    t.Errorf("board.Letters is not right size", len(board.Letters), 5)
+  }
+  if len(board.Letters[0]) != 5 {
+    t.Errorf("board.Letters[0] is not right size", len(board.Letters[0]), 5)
+  }
+  if (board.Letters[0][0] == "") {
+    t.Errorf("board.Letters hasn't been filled in")
   }
 }
 
-func TestWordFileBoardGenerator(t *testing.T) {
-  board := wordFileBoardGenerator(5, "/Users/dan/Dropbox/projects/go/src/goatpress/data/words")
-  if board.Words[0] != "A" {
-    t.Errorf("board.Words[0] not right word", board.Words[0], "A")
-  }
-
-  if len(board.Words) != 235886 {
-    t.Errorf("board.Words not right length", len(board.Words), 235886)
+func TestHashWordSet(t *testing.T) {
+  set := newWordSet()
+  set.Add("hello")
+  set.Add("hi")
+  a1 := set.Includes("hello")
+  if !a1 { t.Errorf("include hello failed", a1, true) }
+  a2 := set.Includes("hippie")
+  if a2 { t.Errorf("include hippie failed", a2, false) }
+  a3 := set.ChooseRandom()
+  if a3 != "hi" && a3 != "hello" {
+    t.Errorf("ChooseRandom didn't choose one")
   }
 }
-
